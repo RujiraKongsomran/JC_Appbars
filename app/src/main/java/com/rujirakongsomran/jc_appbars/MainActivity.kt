cf6a91d208a209f0f68beda4320efa7a614439ff
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-09-13 17:19:30 +0700

    enterAlwaysScrollBehavior

--- app/src/main/java/com/rujirakongsomran/jc_appbars/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_appbars/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
@@ -19,10 +21,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_appbars.ui.theme.JC_AppbarsTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,7 +72,9 @@ fun SmallTopAppBarExample() {
 @Preview(showSystemUi = true)
 @Composable
 fun CenterAlignedTopAppBarExample() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             CenterAlignedTopAppBar(
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
@@ -96,14 +103,19 @@ fun CenterAlignedTopAppBarExample() {
                             contentDescription = "Localized description"
                         )
                     }
-                }
+                },
+                scrollBehavior = scrollBehavior
             )
         }
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
         ) {
-            Text(text = "List item number 1")
+            repeat(200) {
+                Text("Item $it", modifier = Modifier.padding(2.dp))
+            }
         }
     }
 }
